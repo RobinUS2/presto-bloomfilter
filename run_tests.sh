a440ad477e8ab60f62ad718035a25c4d0cdb568e
Robin Verlangen <robin@us2.nl>
2016-10-26 23:35:43 +0200

    Version

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-echo "test2"
+echo "Starting tests"
+
+echo "Installing plugin"
 ./mvnw clean install -Denforcer.skip=true
+
+PRESTO_VERSION=`cat pom.xml | grep '<version>' | head -n 1 | cut -f2 -d">"|cut -f1 -d"<"`
+echo "Presto version $PRESTO_VERSION"
