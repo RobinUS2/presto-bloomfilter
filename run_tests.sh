c43a8dedc10b6e95141ce66670e8ef8babbb1c97
Robin Verlangen <robin@us2.nl>
2016-10-26 22:00:36 +0200

    Cleanup

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 echo "test2"
-cat ~/.m2/settings.xml
-git clone https://github.com/Baqend/Orestes-Bloomfilter.git
-cd Orestes-Bloomfilter
-mvn clean install
-cd ..
 ./mvnw clean install
